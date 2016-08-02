6d9e573443f6e3f2542e71af5b3becd94413b42f
xiazhongda <xzd41@hotmail.com>
2016-08-02 12:50:22 +0800

    small fixes

--- run.sh
+++ run.sh
@@ -58,4 +58,4 @@ kill `ps -ef | grep midar-full | awk '{print $2}'` >/dev/null 2>&1 #kill active 
 [ -d $cwd/$date.run ] && echo "rm -rf $cwd/run-$date"
 [ -d $cwd/$date.run ] && rm -rf $cwd/run-$date #delete pre-existing file.
 echo "$midar --autostep --run-id=$date --dir=$cwd --mper-pps=100 --targets=$cwd/$trace_ip_file start"
-./midar-full --autostep --run-id=$date --dir=$cwd --mper-port=$mper_port --mper-pps=100 --targets=$cwd/$trace_ip_file start
+$midar --autostep --run-id=$date --dir=$cwd --mper-port=$mper_port --mper-pps=100 --targets=$cwd/$trace_ip_file start
