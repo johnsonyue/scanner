6ae58b063d754e9c7d4c8ca7a74d9056d14ebc7b
johnsonyue <johnsonyuehit@163.com>
2017-05-08 09:50:34 +0000

    added cron pull

---
+++ cron.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+[ $# -ne 4 ] && echo './cron.sh $ip $password $src_path $dst_dir' && exit
+ip=$1
+password=$2
+src_path=$3
+dst_dir=$4
+
+fetch(){
+	url=$1
+	src_dir=$(echo $url | awk -F'/' '{sub($NF,"",$0); print $0}')
+	file=$(echo $url | awk -F '/' '{print $NF}' | tr -d '\r')
+	dst_dir=$2
+	cmd="ssh root@$ip \\\"tar zcf - -C $src_dir $file\\\" | tar zxv -C $dst_dir"
+	expect -c "spawn bash -c \"$cmd\"
+	expect -re \".*password.*\" {send \"$password\r\"}
+	expect eof"
+}
+
+cnt=0
+expect -c "spawn ssh root@$ip \"ls $src_path\"
+expect -re \".*password.*\" {send \"$password\r\"}
+expect eof" | while read line; do let cnt=cnt+1; [ $cnt -gt 2 ] && fetch $line $dst_dir; done
