7f89531dc35e9aae11a71fe7214a331079fbd87e
johnsonyue <johnsonyue@163.com>
2017-07-03 06:05:15 +0000

    while true

--- remote.sh
+++ remote.sh
@@ -56,7 +56,7 @@ echo "$iffinder -d -o $out_file_iffinder -c 200 -r 500 $cwd/$trace_ip_file"
 $iffinder -d -o $out_file_iffinder -c 200 -r 500 $cwd/$trace_ip_file
 pwd=$(pwd)
 cd $cwd
-while 1; do [ ! -z "$(ls $cwd | grep 'iffinder.out')" ] && break; sleep 200; done
+while true; do [ ! -z "$(ls $cwd | grep 'iffinder.out')" ] && break; sleep 200; done
 ls $cwd/*iffinder.out | awk -F'/' '{print $NF}' | while read line; do tar zcvf $line.tar.gz $line; done
 cd $pwd
 
